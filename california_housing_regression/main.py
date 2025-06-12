d56b38b28051ce5fbb413e672e157403d4d7a483
Amir Ali Anwarm <amiralianwar611@gmail.com>
2025-06-12 22:05:27 +0500

    working on the Regression algorithum

--- california_housing_regression/main.py
+++ california_housing_regression/main.py
@@ -1,9 +1,23 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
 
+# 1. Load Data
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00242/ENB2012_data.xlsx"
+df  = pd.read_excel(url)
 
-# Load dataset
-df = pd.read_excel(url)
+# 2. Rename columns for readability
+df.columns = [
+    'Relative_Compactness', 'Surface_Area', 'Wall_Area', 'Roof_Area',
+    'Overall_Height', 'Orientation', 'Glazing_Area', 'Glazing_Area_Distribution',
+    'Heating_Load', 'Cooling_Load'
+]
 
-# Show first 5 rows
-print(df.head())
+# 3. Exploratory Data Analysis (EDA)
+print("🔍 Summary statistics:")
+print(df.describe())
+plt.figure()
