10441e6968a1164fba5ff04186fe6334ba48400b
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-06-25 10:26:33 +0500

    Understanding the python

---
+++ Python-Learning/PythonVariables/main.py
@@ -0,0 +1,39 @@
+
+# Many Values to Multiple Variables
+x,y,z= "orange", "yellow","pink"
+
+print(x)
+print(y)
+print(z)
+
+#  One Value to Multiple Variables
+x,y,z= "Amir", "Amir","Amir"
+
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+fruits= ['apple', 'banana','mango']
+x,y,z= fruits
+print(y)
+print(z)
+# Output Variables
+
+
+
+x = "python is awesome"
+
+print(x)
+
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print(x + y + z)
+
+
+
+# Global Variables
