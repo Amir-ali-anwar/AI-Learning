26bf3024d54a0d0ef41df4f3e115b665b500b70b
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-07-09 09:46:48 +0500

    Understanding the NumPy concepts

--- NumPy/ReshapingArrays.py
+++ NumPy/ReshapingArrays.py
@@ -42,4 +42,46 @@ arr6 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
 newarr = arr6.reshape(2, 3, 2)
 
-print(newarr)
+# print(newarr)
+
+
+# Can We Reshape Into any Shape?
+# Yes, as long as the elements required for reshaping are equal in both shapes.
+
+# We can reshape an 8 elements 1D array into 4 elements in 2 rows 2D array but we cannot reshape it
+# into a 3 elements 3 rows 2D array as that would require 3x3 = 9 elements.
+
+
+
+
+
+arr7 = np.array([1,2,3,4,5,6,7])
+# print(arr7[1:5:2])
+
+
+
+# ==================================================================
+
+
+arr8 = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+for x in np.nditer(arr):
+#   print(x)
+  
+  
+  
+# Iterating Array With Different Data Types
+
+ arr9 = np.array([1, 2, 3])
+
+for x in np.nditer(arr9, flags=['buffered'], op_dtypes=['S']):
+# print(x)
+ 
+ 
+ 
+#  Iterating With Different Step Size
+
+  arr10 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+for x in np.nditer(arr10[:, ::2]):
+  print(x)
