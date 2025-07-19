6174e8bc3951b16b29579bd991d6296c30f6870a
Amir Ali Anwar <amiralianwar611@gmil.com>
2025-07-19 20:42:13 +0500

    Data pre-processing

--- covid-data-analysis/main.py
+++ covid-data-analysis/main.py
@@ -1,24 +1,26 @@
 import pandas as pd
-import numpy as np
+# import numpy as np
 
+data = pd.read_csv('../Datasets/covid/covid_19_data.csv')
 
-data= pd.read_csv('../Datasets/covid/covid_19_data.csv')
-# print(data.head())
-# print(data.shape)
-# print(data.info())
-# print(data.describe())
+print(data.head())         
+print(data.shape)         
+print(data.columns)        
+print(data.info())        
+print(data.describe())  
 
-# print(type(data))
-# print(data.isnull().sum())
-# print(data)
-# data['Province/State'] = data.groupby('Country/Region')['Province/State'].ffill().bfill()
-# print(data)
+print(data.isnull().sum())
+
+data['Province/State'] = data.groupby('Country/Region')['Province/State'].ffill().bfill()
+
+data = data.dropna()
+
+data.columns = [col.strip().replace('/', '_').replace(' ', '_') for col in data.columns]
 
 data['ObservationDate'] = pd.to_datetime(data['ObservationDate'])
-print(data.isnull().sum())
-data = data.dropna()  # or fillna if appropriate
 
-country_confirmed= data.groupby('Country/Region')['Confirmed'].sum().sort_values(ascending=False)
+print(data.isnull().sum())
+
+country_confirmed = data.groupby('Country_Region')['Confirmed'].sum().sort_values(ascending=False)
 
 print(country_confirmed.head(5))
-
