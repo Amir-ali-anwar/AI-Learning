c55cc52444868580a98bfcd8f4fe71f502ffdf5d
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-07-26 13:53:49 +0500

    Understanding Lebels

--- LIbraries/Matplotlib/labels.py
+++ LIbraries/Matplotlib/labels.py
@@ -1,11 +1,15 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.plot(x,y)
+# plt.plot(x,y)
 
-plt.xlabel('Average pulse')
-plt.ylabel('Calorie Burnage')
-plt.show()
+# plt.xlabel('Average pulse')
+# plt.ylabel('Calorie Burnage')
+# plt.show()
+
+
+
+# ===============================================================
