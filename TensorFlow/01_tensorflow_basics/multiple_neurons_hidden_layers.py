b734c2000d569988e486108ededa4a035e897885
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-08-09 14:56:31 +0500

    working on the keras

--- TensorFlow/01_tensorflow_basics/multiple_neurons_hidden_layers.py
+++ TensorFlow/01_tensorflow_basics/multiple_neurons_hidden_layers.py
@@ -6,10 +6,12 @@ X = np.array([1, 2, 3, 4, 5], dtype=float)
 Y = np.array([2, 4, 6, 8, 10], dtype=float)
 
 
-modek= keras.Sequential([
+model= keras.Sequential([
     keras.Input(shape=(1,)),
     keras.Dense(units=3, activation='relu'),
     layers.Dense(unit=1)
 ])
 
-model.compile(optimizer='adam' loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit()
